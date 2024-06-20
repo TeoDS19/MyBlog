54417f62cdd9ad9cb5d19c2fd47d6a6aa853d269
Frontend JavaScript <frontendjavascript@iMac-04.local>
2024-06-20 20:45:09 +0300

    myblog

--- backend/models/contact.ts
+++ backend/models/contact.ts
@@ -1,7 +1,6 @@
 import { Contact } from "./../types/Contact";
 import { db } from "../db";
-
-import {  RowDataPacket } from "mysql2";
+import { RowDataPacket } from "mysql2";
 // Get all users
 export const findAll = (callback: Function) => {
   const queryString = `SELECT * FROM contact`;
@@ -17,10 +16,29 @@ export const findAll = (callback: Function) => {
         nume: row.nume,
         prenume: row.prenume,
         email: row.email,
-        mesaj:row.mesaj
+        mesaj: row.mesaj,
       };
       contact_messages.push(contact_message);
     });
     callback(null, contact_messages);
+  });
+};
+
+export const findOne = (contactId: number, callback: Function) => {
+  const queryString = `SELECT * FROM contact WHERE id=?`;
+  db.query(queryString, contactId, (err, result) => {
+    if (err) {
+      callback(err);
+    }
+
+    const row = (<RowDataPacket>result)[0];
+    const message: Contact = {
+      id: row.id,
+      nume: row.nume,
+      prenume: row.prenume,
+      email: row.email,
+      mesaj: row.mesaj,
+    };
+    callback(null, message);
   });
 };
